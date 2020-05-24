cb65a14a192fe3a87293e4fa870227c2cc437085
Egor Blagov <e.m.blagov@gmail.com>
2020-05-24 22:37:25 +0700

    Add basic markup

--- common/l10n.ts
+++ common/l10n.ts
@@ -6,6 +6,10 @@ interface LSchemaMeta {
 
 export const english = {
     language: "Language",
+    chooseDir: "Choose a directory",
+    dirInfo: (filesCount, sizeMb) => `Total files: ${filesCount}, size: ${sizeMb} Mb`,
+    searchExactDoubles: "Search exact doubles",
+    searchCatalogueConflicts: "Search catalogue conflicts",
 };
 
 export type Translation = typeof english;
@@ -16,6 +20,10 @@ assertLanguageSchema(english);
 
 export const russian: Translation = {
     language: "Язык",
+    chooseDir: "Выберите папку",
+    dirInfo: (filesCount, sizeMb) => `Всего файлов: ${filesCount}, размер: ${sizeMb} Мб`,
+    searchExactDoubles: "Искать идеальные дубликаты",
+    searchCatalogueConflicts: "Искать конфликты в каталоге",
 };
 
 export enum Language {
--- ui/components/Main.tsx
+++ ui/components/Main.tsx
@@ -3,7 +3,7 @@ import * as _ from "lodash";
 import * as React from "react";
 import { Language } from "../../common/l10n";
 import { LocalizedProps, withL10n } from "../utils/L10n";
-
+import "./Main.scss";
 interface IOwnProps {
     setLanguage: (l: Language) => void;
 }
@@ -14,18 +14,19 @@ class MainImpl extends React.Component<IProps> {
         this.props.setLanguage(newLanguage);
     };
     render() {
+        const { l10n, lang } = this.props;
         return (
-            <div className="uk-container">
+            <div className="uk-container uk-flex uk-flex-column main-wrapper uk-panel">
                 <div uk-grid="true">
                     <div className="uk-width-expand" />
                     <div className="uk-width-auto">
                         <button className="uk-button uk-button-small" type="button">
-                            {this.props.l10n.language}
+                            {l10n.language}
                         </button>
                         <div uk-dropdown="mode: click" className="uk-padding-small">
                             <ul className="uk-nav uk-dropdown-nav">
                                 {_.map(Object.values(Language), (x) => (
-                                    <li key={x} className={classnames({ "uk-active": x === this.props.lang })}>
+                                    <li key={x} className={classnames({ "uk-active": x === lang })}>
                                         <a href="#" onClick={this.handleChangeLanguage(x)}>
                                             {x}
                                         </a>
@@ -34,6 +35,41 @@ class MainImpl extends React.Component<IProps> {
                             </ul>
                         </div>
                     </div>
+                </div>
+                <hr />
+
+                <div uk-grid="true">
+                    <div className="uk-width-expand">{l10n.chooseDir}</div>
+                    <div className="uk-width-auto">
+                        <button className="uk-button uk-button-small">Open</button>
+                    </div>
+                </div>
+
+                <div uk-grid="true">
+                    <div className="uk-width-expand uk-text-center">{l10n.dirInfo(1000, 1000)}</div>
+                </div>
+
+                <form>
+                    <div className="uk-flex uk-flex-column uk-margin-top">
+                        <label>
+                            <input className="uk-checkbox uk-margin-small-right" type="checkbox" />
+                            {l10n.searchExactDoubles}
+                        </label>
+                        <label>
+                            <input className="uk-checkbox uk-margin-small-right" type="checkbox" />
+                            {l10n.searchCatalogueConflicts}
+                        </label>
+                    </div>
+                </form>
+                <progress className="uk-progress" value="10" max="100" />
+                <div className="uk-flex-1 uk-panel-scrollable">
+                    <ul className="uk-list uk-list-divider uk-list-striped uk-list-large">
+                        {_.map([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0], (x, i) => (
+                            <li key={i}>
+                                File {x} {i}
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </div>
         );
