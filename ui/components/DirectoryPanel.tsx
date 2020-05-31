86c8e4dcb452075fd7cc5ff6f9932989992df2f6
Egor Blagov <e.m.blagov@gmail.com>
2020-05-31 19:54:39 +0700

    Rewrite directory panel with hooks
    - add error handling

--- ui/components/DirectoryPanel.tsx
+++ ui/components/DirectoryPanel.tsx
@@ -3,59 +3,70 @@ import { remote } from "electron";
 import * as React from "react";
 import { ipc } from "../../common/ipc";
 import { isOk } from "../../common/tools";
-import { LocalizedProps, withL10n } from "../utils/L10n";
+import { useL10n } from "../utils/L10n";
+import { useNotification } from "../utils/notifications";
 import "./DirectoryPanel.css";
 import { DirectorySummary } from "./DirectorySummary";
 
-type IProps = LocalizedProps<{}>;
+export const DirectoryPanel = () => {
+    const [l10n, _] = useL10n();
+    const notification = useNotification();
 
-interface IState {
-    path: string;
-    filesCount: number;
-    sizeMb: number;
-}
+    const [path, setPath] = React.useState<string>();
+    const [filesCount, setFilesCount] = React.useState<number>();
+    const [sizeMb, setSizeMb] = React.useState<number>();
+    const [dialogOpen, setDialogOpen] = React.useState<boolean>(false);
 
-class DirectoryPanelImpl extends React.Component<IProps, IState> {
-    state: IState = {
-        path: undefined,
-        filesCount: undefined,
-        sizeMb: undefined,
-    };
-
-    handleOpenDialog = () => {
-        remote.dialog.showOpenDialog({ properties: ["openDirectory"] }).then((res) => {
-            this.setState({ path: res.filePaths && res.filePaths[0] });
-        });
-    };
-
-    componentDidUpdate(_: IProps, prevState: IState) {
-        if (isOk(this.state.path) && this.state.path !== prevState.path) {
-            this.setState({ filesCount: undefined, sizeMb: undefined });
-            ipc.renderer.rpc.getDirectoryInfo({ targetPath: this.state.path }).then((result) => {
-                this.setState({ filesCount: result.filesCount, sizeMb: result.sizeMb });
-            });
+    React.useEffect(() => {
+        if (isOk(path)) {
+            setFilesCount(undefined);
+            setSizeMb(undefined);
+            setDialogOpen(true);
+            ipc.renderer.rpc
+                .getDirectoryInfo({ targetPath: path })
+                .then((result) => {
+                    setFilesCount(result.filesCount);
+                    setSizeMb(result.sizeMb);
+                })
+                .catch((error: Error) => {
+                    notification.showError(l10n.errorPath(error.message));
+                    setPath(undefined);
+                })
+                .finally(() => {
+                    setDialogOpen(false);
+                });
         }
-    }
+    }, [path]);
 
-    render() {
-        const { l10n } = this.props;
-        const { path, filesCount, sizeMb } = this.state;
+    const handleOpenDialog = () => {
+        setDialogOpen(true);
+        remote.dialog
+            .showOpenDialog({ properties: ["openDirectory"] })
+            .then((res) => {
+                if (!res.canceled && res.filePaths[0] !== path) {
+                    setPath(res.filePaths[0]); // TODO: on path change settings also change due to stack-like design of panels
+                    ipc.renderer.rpc.interruptSearch(); // it's Async
+                }
+                setDialogOpen(false);
+            })
+            .catch((error: Error) => {
+                notification.showError(l10n.errorOpenPath(error.message));
+            });
+    };
 
-        return (
-            <>
-                <Box display="flex" my={1}>
-                    <Box flexGrow={1} display="flex" flexDirection="column" justifyContent="center">
-                        <Typography className="directory-panel__caption">
-                            {isOk(path) ? path : l10n.chooseDir}
-                        </Typography>
-                    </Box>
-                    <Box>
-                        <Button onClick={this.handleOpenDialog}>{l10n.open}</Button>
-                    </Box>
+    return (
+        <>
+            <Box display="flex" my={1}>
+                <Box flexGrow={1} display="flex" flexDirection="column" justifyContent="center">
+                    <Typography className="directory-panel__caption">{isOk(path) ? path : l10n.chooseDir}</Typography>
                 </Box>
-                {isOk(path) && <DirectorySummary targetPath={path} filesCount={filesCount} sizeMb={sizeMb} />}
-            </>
-        );
-    }
-}
-export const DirectoryPanel = withL10n<{}>(DirectoryPanelImpl);
+                <Box>
+                    <Button onClick={handleOpenDialog} disabled={dialogOpen}>
+                        {l10n.open}
+                    </Button>
+                </Box>
+            </Box>
+            {isOk(path) && <DirectorySummary targetPath={path} filesCount={filesCount} sizeMb={sizeMb} />}
+        </>
+    );
+};
