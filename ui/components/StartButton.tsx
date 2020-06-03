3da70b0a611c1e2cdaa7cc6d92ec8e8cc4b1b89a
Egor Blagov <e.m.blagov@gmail.com>
2020-06-03 22:49:46 +0700

    Return fancy transitions to Start button
    - current approach excludes Button remounting
    - <span> required by Tooltip to make it work on disabled button
    - () => null -- is hack to disable tooltip on enabled button
    - undefined is default popper behavior

--- ui/components/StartButton.tsx
+++ ui/components/StartButton.tsx
@@ -12,19 +12,14 @@ export const StartButton = ({ onClick, params }: IProps) => {
     const [l10n, _] = useL10n();
 
     const canStart = params.searchMd5 || params.searchTgi;
-    const Wrapper = canStart
-        ? React.Fragment
-        : (props) => (
-              <Tooltip title={l10n.enableForSearch}>
-                  <span>{props.children}</span>
-              </Tooltip>
-          );
-
+    const popper = canStart ? () => null : undefined; // => null is to disable tooltip
     return (
-        <Wrapper>
-            <Button color="primary" variant="contained" onClick={onClick} disabled={!canStart}>
-                {l10n.start}
-            </Button>
-        </Wrapper>
+        <Tooltip title={l10n.enableForSearch} PopperComponent={popper}>
+            <span>
+                <Button color="primary" variant="contained" onClick={onClick} disabled={!canStart}>
+                    {l10n.start}
+                </Button>
+            </span>
+        </Tooltip>
     );
 };
