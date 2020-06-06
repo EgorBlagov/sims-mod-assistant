b5599f74c192973d42f2d1c17f90592c77bc315e
Egor Blagov <e.m.blagov@gmail.com>
2020-06-06 18:21:45 +0700

    Fix summary files reducer

--- ui/components/files-area/FilesArea.tsx
+++ ui/components/files-area/FilesArea.tsx
@@ -33,7 +33,7 @@ export const FilesArea = ({ searchInfo }: IProps) => {
     };
 
     const duplicatesCount = isOk(searchInfo)
-        ? _.reduce(searchInfo.duplicates, (sum, entry) => (sum += entry.duplicates.length), 0)
+        ? _.reduce(searchInfo.duplicates, (sum, entry) => sum + entry.duplicates.length, 0)
         : 0;
     const skipsCount = isOk(searchInfo) ? searchInfo.skips.length : 0;
 
