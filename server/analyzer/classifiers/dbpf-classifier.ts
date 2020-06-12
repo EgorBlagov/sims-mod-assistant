05392914e7d0d3dec4ca54b739688d7ded8d0cda
Egor Blagov <e.m.blagov@gmail.com>
2020-06-13 00:08:40 +0700

    Add support of several object definitions within package

--- server/analyzer/classifiers/dbpf-classifier.ts
+++ server/analyzer/classifiers/dbpf-classifier.ts
@@ -1,5 +1,4 @@
 import * as _ from "lodash";
-import { isOk } from "../../../common/tools";
 import { IDbpfRecord, readDbpf } from "../../dbpf";
 import { DbpfResourceTypes } from "../../dbpf/constants";
 import { IFileClassifier } from "./file-classifier";
@@ -7,20 +6,11 @@ import { IFileClassifier } from "./file-classifier";
 export class DbpfClassifier implements IFileClassifier {
     async getKeys(path: string): Promise<string[]> {
         const dbpf = await readDbpf(path);
-
-        const result: string[] = [];
-        const dbpfCatalog = _.filter(dbpf.records, (r) => r.resourceType === DbpfResourceTypes.Catalog)[0];
-        const dbpfDefinition = _.filter(dbpf.records, (r) => r.resourceType === DbpfResourceTypes.Definition)[0];
         const toTgi = (rec: IDbpfRecord): string => `${rec.resourceType}${rec.resourceGroup}${rec.instance}`;
-
-        if (isOk(dbpfCatalog)) {
-            result.push(toTgi(dbpfCatalog));
-        }
-
-        if (isOk(dbpfDefinition)) {
-            result.push(toTgi(dbpfDefinition));
-        }
-
-        return result;
+        const typesToCheck = [DbpfResourceTypes.Catalog, DbpfResourceTypes.Definition];
+        return _(dbpf.records)
+            .filter((r) => typesToCheck.includes(r.resourceType))
+            .map(toTgi)
+            .value();
     }
 }
