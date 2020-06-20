8953b1514664076fe4236292bda0d3d2bfa78261
Egor Blagov <e.m.blagov@gmail.com>
2020-06-20 21:06:14 +0700

    Decompose d3 graph rendering
    - not typing

--- ui/components/files-area/duplicates/detailed/D3Graph.tsx
+++ ui/components/files-area/duplicates/detailed/D3Graph.tsx
@@ -46,6 +46,9 @@ const styles = (theme: Theme) =>
             strokeWidth: 1.5,
             strokeOpacity: 0.6,
         },
+        svg: {
+            fontFamily: "sans-serif",
+        },
     });
 
 interface IProps extends WithStyles<typeof styles> {
@@ -59,133 +62,161 @@ export class D3GraphImpl extends React.Component<IProps> {
     private d3RootRef = createRef<HTMLDivElement>();
 
     componentDidMount() {
-        const drag = (simulation) => {
-            const dragstarted = (d) => {
-                if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-                d.fx = d.x;
-                d.fy = d.y;
-            };
+        const { graph: data, classes, l10n } = this.props;
 
-            const dragged = (d) => {
-                d.fx = d3.event.x;
-                d.fy = d3.event.y;
-            };
+        const links = data.links.map((d) => Object.create(d));
+        const nodes = data.nodes.map((d) => Object.create(d));
 
-            const dragended = (d) => {
-                if (!d3.event.active) simulation.alphaTarget(0);
-                d.fx = null;
-                d.fy = null;
-            };
+        const simulation = this.createSimulation(nodes, links);
+        const svg = this.createSvg();
+        const tooltip = this.createTooltip();
+        const link = svg.append("g").attr("class", classes.link).selectAll("line").data(links).join("line");
+        const node = this.createNode(svg, nodes, simulation);
+        const linkLabel = this.createLinkLabel(svg, links, tooltip);
 
-            return d3.drag().on("start", dragstarted).on("drag", dragged).on("end", dragended);
-        };
+        simulation.on("tick", () => {
+            link.attr("x1", (d) => d.source.x)
+                .attr("y1", (d) => d.source.y)
+                .attr("x2", (d) => d.target.x)
+                .attr("y2", (d) => d.target.y);
 
-        const { graph: data, width, height, classes, l10n } = this.props;
+            node.attr("transform", (d) => `translate(${d.x},${d.y})`);
 
-        const chart = (mountpoint) => {
-            const links = data.links.map((d) => Object.create(d));
-            const nodes = data.nodes.map((d) => Object.create(d));
-
-            const simulation = d3
-                .forceSimulation(nodes)
-                .force(
-                    "link",
-                    d3.forceLink(links).id((d: any) => d.path),
-                )
-                .force("charge", d3.forceManyBody().strength(-5000))
-                .force("x", d3.forceX())
-                .force("y", d3.forceY());
-
-            const svg = mountpoint
-                .append("svg")
-                .attr("viewBox", [-width / 2, -height / 2, width, height])
-                .style("font-family", "sans-serif");
-
-            const tooltip = mountpoint
-                .append("div")
-                .attr("class", classes.tooltip)
-                .style("opacity", 0)
-                .style("left", 0)
-                .style("top", 0)
-                .on("mouseover", () => {
-                    tooltip.transition().duration(200).style("opacity", 1).style("pointer-events", "all");
-                })
-                .on("mouseout", () => {
-                    tooltip.transition().duration(200).style("opacity", 0).style("pointer-events", "none");
-                });
-
-            const link = svg.append("g").attr("class", classes.link).selectAll("line").data(links).join("line");
-
-            const node = svg.append("g").selectAll("g").data(nodes).join("g").call(drag(simulation));
-
-            node.append("circle").attr("class", classes.nodeCircle).attr("r", 10);
-
-            node.append("text")
-                .attr("x", "1em")
-                .text((d) => path.basename(d.path))
-                .clone(true)
-                .lower()
-                .attr("class", classes.labelBorder);
-
-            const linkLabel = svg.append("g").selectAll("g").data(links).join("g");
-
-            linkLabel
-                .append("text")
-                .attr("x", 0)
-                .attr("y", 0)
-                .attr("class", classes.linkLabel)
-                .text((d) => d.types.join(", "))
-                .on("mouseover", (d) => {
-                    tooltip.transition().duration(200).style("opacity", 1).style("pointer-events", "all");
-
-                    const rootRect = this.d3RootRef.current.getBoundingClientRect();
-                    const labelRect = d3.event.target.getBoundingClientRect();
-                    const x = labelRect.x + labelRect.width - rootRect.left;
-                    const y = labelRect.y + labelRect.height - rootRect.top;
-                    tooltip
-                        .html(d.keys.join("<br/>"))
-                        .style("left", x + "px")
-                        .style("top", y + "px");
-                })
-                .on("mouseout", () => {
-                    tooltip.transition().duration(200).delay(200).style("opacity", 0).style("pointer-events", "none");
-                });
-
-            linkLabel
-                .selectAll("text")
-                .clone(true)
-                .lower()
-                .attr("class", [classes.labelBorder, classes.linkLabel].join(" "));
-
-            simulation.on("tick", () => {
-                link.attr("x1", (d) => d.source.x)
-                    .attr("y1", (d) => d.source.y)
-                    .attr("x2", (d) => d.target.x)
-                    .attr("y2", (d) => d.target.y);
-                node.attr("transform", (d) => `translate(${d.x},${d.y})`);
-                linkLabel.attr(
-                    "transform",
-                    (d) => `translate(${(d.source.x + d.target.x) / 2},${(d.source.y + d.target.y) / 2})`,
-                );
-            });
-
-            return svg.node();
-        };
-
-        const vis = d3.select(this.d3RootRef.current);
-        chart(vis);
+            linkLabel.attr(
+                "transform",
+                (d) => `translate(${(d.source.x + d.target.x) / 2},${(d.source.y + d.target.y) / 2})`,
+            );
+        });
     }
 
     componentDidUpdate() {
-        const { width, height } = this.props;
-        d3.select(this.d3RootRef.current)
-            .select("svg")
-            .attr("viewBox", [-width / 2, -height / 2, width, height] as any);
+        this.selectRoot().select("svg").attr("viewBox", this.getViewBox());
     }
+
+    private selectRoot = () => {
+        return d3.select(this.d3RootRef.current);
+    };
+
+    private getViewBox = (): any => {
+        const { width, height } = this.props;
+        return [-width / 2, -height / 2, width, height];
+    };
+
+    private getDrag = (simulation: any) => {
+        const dragstarted = (d) => {
+            if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+            d.fx = d.x;
+            d.fy = d.y;
+        };
+
+        const dragged = (d) => {
+            d.fx = d3.event.x;
+            d.fy = d3.event.y;
+        };
+
+        const dragended = (d) => {
+            if (!d3.event.active) simulation.alphaTarget(0);
+            d.fx = null;
+            d.fy = null;
+        };
+
+        return d3.drag().on("start", dragstarted).on("drag", dragged).on("end", dragended);
+    };
+
+    private createSimulation = (nodes: any[], links: any[]) => {
+        return d3
+            .forceSimulation(nodes)
+            .force(
+                "link",
+                d3.forceLink(links).id((d: any) => d.path),
+            )
+            .force("charge", d3.forceManyBody().strength(-5000))
+            .force("x", d3.forceX())
+            .force("y", d3.forceY());
+    };
+
+    private createSvg = () => {
+        const { classes } = this.props;
+        return this.selectRoot().append("svg").attr("viewBox", this.getViewBox()).attr("class", classes.svg);
+    };
+
+    private createTooltip = () => {
+        const { classes } = this.props;
+        const tooltip = this.selectRoot()
+            .append("div")
+            .attr("class", classes.tooltip)
+            .style("opacity", 0)
+            .style("left", 0)
+            .style("top", 0)
+            .on("mouseover", () => {
+                tooltip.transition().duration(200).style("opacity", 1).style("pointer-events", "all");
+            })
+            .on("mouseout", () => {
+                tooltip.transition().duration(200).style("opacity", 0).style("pointer-events", "none");
+            });
+
+        return tooltip;
+    };
+
+    private createNode = (svg: any, nodes: any[], simulation: any) => {
+        const { classes } = this.props;
+
+        const node = svg
+            .append("g")
+            .selectAll("g")
+            .data(nodes)
+            .join("g")
+            .call(this.getDrag(simulation) as any);
+
+        node.append("circle").attr("class", classes.nodeCircle).attr("r", 10);
+
+        node.append("text")
+            .attr("x", "1em")
+            .text((d) => path.basename(d.path))
+            .clone(true)
+            .lower()
+            .attr("class", classes.labelBorder);
+
+        return node;
+    };
+
+    private createLinkLabel = (svg: any, links: any[], tooltip: any) => {
+        const { classes } = this.props;
+        const linkLabel = svg.append("g").selectAll("g").data(links).join("g");
+
+        linkLabel
+            .append("text")
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("class", classes.linkLabel)
+            .text((d) => d.types.join(", "))
+            .on("mouseover", (d) => {
+                tooltip.transition().duration(200).style("opacity", 1).style("pointer-events", "all");
+
+                const rootRect = this.d3RootRef.current.getBoundingClientRect();
+                const labelRect = d3.event.target.getBoundingClientRect();
+                const x = labelRect.x + labelRect.width - rootRect.left;
+                const y = labelRect.y + labelRect.height - rootRect.top;
+                tooltip
+                    .html(d.keys.join("<br/>"))
+                    .style("left", x + "px")
+                    .style("top", y + "px");
+            })
+            .on("mouseout", () => {
+                tooltip.transition().duration(200).delay(200).style("opacity", 0).style("pointer-events", "none");
+            });
+
+        linkLabel
+            .selectAll("text")
+            .clone(true)
+            .lower()
+            .attr("class", [classes.labelBorder, classes.linkLabel].join(" "));
+
+        return linkLabel;
+    };
 
     render() {
         const { height, width, classes } = this.props;
-
         return <div style={{ height, width }} className={classes.container} ref={this.d3RootRef} />;
     }
 }
